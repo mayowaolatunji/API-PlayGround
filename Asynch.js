136176c013ef46cf8fe093abd99f0f00e7e0aa14
Mayowa Olatunji <olatunjimayowa0396@gmail.com>
2023-10-26 11:00:40 +0100

    Now Studying Class

--- Asynch.js
+++ Asynch.js
@@ -1,8 +1,20 @@
-const getDog = async() => {
-    const url = 'https://dog.ceo/api/breeds/image/random'
-    const response = await fetch(url);
-    const data = await response.json()
-    console.log(data);
-}
 
-getDog()
+class car {
+
+    constructor (name, speed, color){
+        this.name = name;
+        this.speed = speed;
+        this.color = color
+    }
+
+    drive(){
+        console.log("the car is on Drive")
+    }
+
+}
+ const ferrari = new car('Ferrari', 300,'red');
+ console.log(ferrari.color)
+
+ ferrari.drive()
+
+ 
