e97a118bb83d936ed7ff30dc36b92d29a5e313bd
Mayowa Olatunji <olatunjimayowa0396@gmail.com>
2023-10-30 21:44:58 +0100

    Just debugging left.

--- Asynch.js
+++ Asynch.js
@@ -1,59 +1,65 @@
 const input = document.querySelector(".input");
-const search = document.querySelector(".search");
+const search = document.querySelector("#search"); // Fix the id selector here
+const dogContainer = document.querySelector('.dog-container');
+const dog = document.querySelector('.dog'); // Select the '.dog' div to add new images
 
+// Function to fetch a random dog image based on the search input
+const browseDog = async (browse) => {
+  const link = `https://dog.ceo/api/breed/${browse}/images/random`;
+  const response = await fetch(link);
+  const data = await response.json();
+  const imageUrl = data.message;
 
-// we want the search button to take user input to search for dogs using their calls.
+  if (data.status === "error") {
+    // Handle the case where no image is found for the given breed
+    alert("No dog image found for the specified breed.");
+  } else {
+    // Create a new image element
+    const newImage = document.createElement('img');
+    newImage.src = imageUrl;
+    newImage.height = 300;
+    newImage.width = 300;
+
+    // Add the new image to the 'dog' div
+    dog.appendChild(newImage);
+
+    const maxImages = 8; // Adjust this number based on your container size
+    if (dogContainer.children.length >= maxImages) {
+      // Container is filled, remove the first image
+      dogContainer.firstElementChild.remove();
+    }
+  }
+};
 
 search.addEventListener("click", function () {
-    const browse = input.value;
-  
-    // Function to fetch a random dog image based on the search input
-    const browseDog = async () => {
-      const link = `https://dog.ceo/api/breed/${browse}/images/random`;
-      const response = await fetch(link);
-      const data = await response.json();
-      const imageUrl = data.message;
-  
-      if (data.status === "error") {
-        // Handle the case where no image is found for the given breed
-        alert("No dog image found for the specified breed.");
-      } else {
-        dog.innerHTML += `<img src="${imageUrl}" height="300" width="300" />`;
-      }
-    };
-  
-    browseDog();
-  });
+  const browse = input.value;
+  browseDog(browse);
+});
 
-
-   
-
-
-
-const body = document.querySelector("body");
 const getNewDog = document.querySelector(".getNewDog");
-const dog = document.querySelector(".dog");
-body.append(dog)
 
 const getDog = async () => {
-    const url = "https://dog.ceo/api/breeds/image/random";
-    const response = await fetch(url);
-    const data = await response.json();
-    const imageUrl = data.message; // Assuming the URL is in the "message" property
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const response = await fetch(url);
+  const data = await response.json();
+  const imageUrl = data.message;
 
-    console.log(imageUrl);
-    dog.innerHTML += `<img src="${imageUrl}" height =300 width = 300 />`;
-}
+  // Create a new image element
+  const newImage = document.createElement('img');
+  newImage.src = imageUrl;
+  newImage.height = 300;
+  newImage.width = 300;
 
-getNewDog.addEventListener("click", function(){
-    getDog()
+  // Add the new image to the 'dog' div
+  dog.appendChild(newImage);
 
-} )
+  const maxImages = 8; // Adjust this number based on your container size
+  if (dogContainer.children.length >= maxImages) {
+    // Container is filled, remove the first image
+    dogContainer.firstElementChild.remove();
+  }
+};
 
-
-
-
-
-
-
-
+getNewDog.addEventListener("click", function () {
+  getDog();
+});
